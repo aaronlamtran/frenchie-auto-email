d9fcca0a9d9465e1e52f565081a3a26a039eca18
Aaron Tran <altran32@gmail.com>
2022-07-12 23:29:42 -0700

    connect db, find, read

---
+++ auto_email.py
@@ -0,0 +1,30 @@
+from curses import curs_set
+from pymongo import MongoClient
+import pymongo
+from dotenv import load_dotenv
+import os
+load_dotenv()
+MONGO_USER = os.getenv('MONGO_USER')
+MONGO_PW = os.getenv('MONGO_PW')
+CONNECTION_STRING = f"mongodb+srv://{MONGO_USER}:{MONGO_PW}@frenchie-connection-db.nxc9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+def main():
+
+
+    cluster = MongoClient(CONNECTION_STRING)
+    main_db = cluster.list_database_names()[0]
+    frenchie_collection = cluster[main_db]
+    all_collections = frenchie_collection.list_collection_names()
+    waitlists = frenchie_collection.get_collection('waitlists')
+    cursor = waitlists.find({})
+
+    for document in cursor:
+      print(document)
+
+    for collection in all_collections:
+      print(collection)
+
+
+if __name__ == "__main__":
+    main()
